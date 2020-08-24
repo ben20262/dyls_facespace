f195b34d1309d7dd55c156892971b9e2a664fa1a
ben20262 <ben20262@aim.com>
2020-08-23 21:09:26 -0400

    Added for messenger

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -1,8 +1,8 @@
-require 'facebook/messenger'
+# require 'facebook/messenger'
 
-include Facebook::messenger
-Facebook::Messenger::Subscriptions.subscribe(access_token:ENV["FACEBOOK_KEY"])
+# include Facebook::messenger
+# Facebook::Messenger::Subscriptions.subscribe(access_token:ENV["FACEBOOK_KEY"])
 
-Bot.on :message do |message|
-    puts "got your message!"
-end
+# Bot.on :message do |message|
+#     puts "got your message!"
+# end
